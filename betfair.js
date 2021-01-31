91522138f5fb8e6b60890cc5ce83b37172c606b4
gabriel <gabriel.casascorral@host.docker.internal>
2021-01-31 17:41:51 +0100

    example betfair

--- betfair.js
+++ betfair.js
@@ -1,7 +1,12 @@
 
 var asyncMaker=function () { 
         return async function () {
-            await page.goto('https://www.google.es');
+            await page.goto('https://www.betfair.es/sport/inplay');
+
+            await page.waitForSelector('#ssc-liu');
+            await page.click('#ssc-liu');
+            await  page.keyboard.sendCharacter("a");
+
 
         };};
 

638afb42ce3f5044450a936075d77a96a2e90594
Gabriel <gabicasas@gmail.com>
2021-02-01 23:19:04 +0100

    cod simplify

--- betfairMapper.ts
+++ betfairMapper.ts
@@ -1,24 +1,6 @@
 if(Array.isArray(dataNormalized) && dataNormalized.length>0 && dataNormalized[0].marketId){
     //console.log("mercados"); 
+    console.log("HHHEEERRREEE");
     
-    
-    /*********Esto va a un mapper que será codigo personalizado *********** */
-    for(let i in dataNormalized){
-        let mh: MarketHistory = new MarketHistory();
-        mh.marketId=dataNormalized[i].marketId;
-        mh.marketStatus=dataNormalized[i].marketStatus;
-        mh.timestamp=new Date();
-        mh.runnerDetails=[];
-        let runnerDetails: any[] =dataNormalized[i].runnerDetails;
-        runnerDetails.forEach(runnerDetail => {
-            let rDetail: RunnerDetail = new RunnerDetail();
-            rDetail.runnerStatus = runnerDetail.runnerStatus;
-            rDetail.selectionId = runnerDetail.selectionId;
-            //No estoy seguro que esta cuota sea correcta
-            rDetail.quota = runnerDetail.runnerOdds.trueOdds.decimalOdds.decimalOdds;
-            mh.runnerDetails.push(rDetail);
-         });
-     /* ******************************************************************* */
-        this.marketHRepository.save(mh).then(result => {/* console.log("Saved")*/;});
-    } 
+   
    }
